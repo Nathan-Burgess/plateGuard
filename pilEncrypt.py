4288c2be42118f62645b3f9f786abd9768829e4b
Nathan <nathan.burgess@comcast.net>
2018-06-04 13:34:44 -0500

    Made pilTest to a function and renamed pilEncrypt

---
+++ pilEncrypt.py
@@ -0,0 +1,63 @@
+# Import the modules
+from PIL import Image
+import random
+
+
+def pillEncrypt(plate):
+    try:
+
+        # Load an image from the hard drive
+        original = Image.open("C:/Users/natha/Desktop/car_plate.jpg")
+
+        # loading pixel values
+        pixels = original.load()
+
+        #licsense plate string
+        LP = plate['plate']
+
+        # seed from asci values of LP
+        seed = 0
+        for p in range(0, len(LP)):
+            seed = seed + ord(LP[p])
+
+        #coordinates of top left and bottom right points
+        coordinates = plate['coordinates']
+        a = coordinates[0]
+        b = coordinates[2]
+        x1 = a['x']
+        x2 = b['x']
+        y1 = a['y']
+        y2 = b['y']
+
+        random.seed(seed)
+
+        original.show()
+
+        #encypting image plate
+        for x in range(x1, x2, ):
+            for y in range(y1, y2, ):
+                r, g, b = original.getpixel((x, y))
+                pixels[x, y] = ((r ^ random.randint(1, 255)), (g ^ random.randint(1, 255)), (b ^ random.randint(1, 255)))
+
+        # Display encrypted
+        original.show()
+
+        #reseding using seed value
+        random.seed(seed)
+
+        #unencrypting plate number
+        for x in range(x1, x2, ):
+            for y in range(y1, y2, ):
+                r, g, b = original.getpixel((x, y))
+                pixels[x, y] = ((r ^ random.randint(1, 255)), (g ^ random.randint(1, 255)), (b ^ random.randint(1, 255)))
+
+
+        ##displaying unecrypted image
+        original.show()
+
+
+
+
+
+    except:
+        print("Error somewhere")
