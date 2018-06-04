11dd476b80f2b10a9fb50d35497069a9698957fa
Michael Nutt <michaelnutt2@my.unt.edu>
2018-06-03 22:01:42 -0500

    Removed unneeded code from the test2.py, don't need to loop through candidates, only need to loop through the plates. Having it print the plate number and the coordinates of the found plate. Was not able to test test2.py but same code is working in test.py

--- test2.py
+++ test2.py
@@ -42,13 +42,8 @@ try:
         for plate in results['results']:
             i += 1
             print("Plate #%d" % i)
-            print("   %12s %12s" % ("Plate", "Confidence"))
-            for candidate in plate['candidates']:
-                prefix = "-"
-                if candidate['matches_template']:
-                    prefix = "*"
-
-                print("  %s %12s%12f" % (prefix, candidate['plate'], candidate['confidence']))
+            print("   %12s %12s" % ("Plate", "Coordinates"))
+            print("   %12s %12s" % (plate['plate'], plate['coordinates']))
 
 
 
