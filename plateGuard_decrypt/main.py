a1bf1e556b6eaa55a0df27d5014e0adf667c4081
Michael Nutt <michaelnutt2@my.unt.edu>
2018-06-04 19:36:08 -0500

    Added function call to main for decryptor

--- plateGuard_decrypt/main.py
+++ plateGuard_decrypt/main.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from pilDecrypt import *
 
 with open("config.json", "r") as read_file:
     config = json.load(read_file)
@@ -8,4 +9,4 @@ if len(sys.argv) < 3:
     print("Usage: main.py <license plate> <image name>")
     sys.exit(1)
 
-#TODO Add in the function call for decrypting, will take arguments: sys.arg[1] sys.arg[2]
+pilDecrypt(sys.arg[1], sys.arg[2])
