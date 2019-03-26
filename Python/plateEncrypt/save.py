6d93d2ae0e7f758a4282d734310c378bf0cc1e40
Unknown <nathan.burgess@comcast.net>
2019-03-26 13:45:40 -0500

    Added save function

--- Python/plateEncrypt/save.py
+++ Python/plateEncrypt/save.py
@@ -1,7 +1,14 @@
 
-#TODO add ability to put metadata to front of frame showing time-data, also add file namin convention
+
+# TODO add ability to put metadata to front of frame showing time-data, also add file namin convention
 def save_encypt(ciphertext, tag, nonce):
 
     file_out = open("encrypted.txt", "wb")
     [file_out.write(x) for x in (ciphertext, tag, nonce)]
     print(ciphertext + tag + nonce)
+
+
+def save_frame(buf, out):
+
+    for b in buf.frames:
+        out.write(b)
