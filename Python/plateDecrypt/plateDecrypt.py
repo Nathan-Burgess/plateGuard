c6fed4ee83b03c639fc0e915f54959b60e312029
Unknown <nathan.burgess@comcast.net>
2019-04-02 13:16:20 -0500

    pushing mim variables for decrypt

--- Python/plateDecrypt/plateDecrypt.py
+++ Python/plateDecrypt/plateDecrypt.py
@@ -1,8 +1,18 @@
 
+import cv2
+import read
+
 
 def main():
 
-    print("HI EARTH")
+    read.read_encrypted()
+    path = input("Enter path")
+
+    cap = cv2.VideoCapture(path)
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    # Set up output file
+    out = cv2.VideoWriter('../Unencrypted/output.avi', fourcc, 30, (3840, 2160))
 
 
 if __name__ == "__main__":
---
+++ Python/plateDecrypt/read.py
@@ -0,0 +1,10 @@
+import glob
+import unittest
+
+
+def read_encrypted():
+
+    files = glob.glob("../20190401/0000/*.wfc")
+    print (files)
+
+
