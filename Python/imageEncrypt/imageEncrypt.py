ddac3510f166cd42854b914e32d257a5e7163543
Unknown <nathan.burgess@comcast.net>
2019-04-18 13:44:35 -0500

    Cleaned up Image Encrypt

--- Python/imageEncrypt/imageEncrypt.py
+++ Python/imageEncrypt/imageEncrypt.py
@@ -9,53 +9,42 @@ from Crypto.Random import get_random_bytes
 def main():
     # Read from file to import video
     cap = cv2.VideoCapture("../../test_plates/test_video3.mp4")
-    # Something for writing out the video, codec related probably
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    # Set up output file
-    out = cv2.VideoWriter('../imageFiles/output.avi', fourcc, 30, (3840, 2160))
 
+    #Setting up Socket connection to server
     s = connection.start_connection()
     host_ip = connection.connect_to_host("vmware.cse.unt.edu", 3000, s)
+
     print("the socket has successfully connected")
 
+    #generating halof of the cipher key
     key = get_random_bytes(16)
-    print(key)
+
+    #sending half of key to server
     connection.send_message(key, s)
 
+    #recieving full key
     key = connection.recv_message(32, s)
-    print(key)
 
+    print(key)
     ret = True
-    # cipher = encrypt.set_salsa()
+
+    #Setting up cipher with correct key
     cipher = encrypt.set_chacha(key)
 
 
     while ret:
         for i in range(300):
             ret, frame = cap.read()
-            print(frame)
-            cv2.imwrite("origional.jpg", frame)
             if ret is True:
-               # encrypt.encrypt_salsa(frame, cipher)
-               # encrypt.encrypt_AES(frame)
-                data = cv2.imencode('.jpg', frame)[1].tostring()
-                data = data + str.encode("halo")
+                #encrypting frame
                 output = encrypt.encrypt_chacha(frame, cipher)
+                #adding ending terminator
                 output = output + str.encode("halo")
+                #sending frame
                 connection.send_message(output, s)
             else:
                 break
 
-        print("Finding Plates...NOTTTTTTTT")
-       # nounce = output[:8]
-       # ciphertext = output[8:]
-       # cipher = ChaCha20.new(key=key, nonce=nounce)
-       # decoded = cipher.decrypt(ciphertext)
-        #frame2 = cv2.imdecode(decoded, cv2.IMREAD_COLOR)
-        #frame2 = cv2.imdecode(np.frombuffer(data, np.uint8), -1)
-        #cv2.imwrite("deco2ded.jpg", frame2)
-        #print(frame2)
-        print(data)
         ret = False
 
 
