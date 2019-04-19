4c71965f13273e29e89e196a759d79fe65e6c621
Michael Nutt <michaelnutt2@my.unt.edu>
2019-04-19 11:04:03 -0500

    I don't even know anymore

--- Python/imageEncrypt/imageEncrypt.py
+++ Python/imageEncrypt/imageEncrypt.py
@@ -38,7 +38,7 @@ def main():
                 # Encrypting frame
                 output = encrypt.encrypt_chacha(frame, cipher)
                 # adding ending terminator
-                connection.send_message(len(output))
+                connection.send_message(len(output), s)
                 # output = output + str.encode("halo")
                 print("Sending frame " + str(i+1))
                 connection.send_message(output, s)
