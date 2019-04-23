4b1569e96902eef97c61bf8f80959e2998dead73
Michael Nutt <michaelnutt2@my.unt.edu>
2019-04-23 14:48:56 -0500

    Try again

--- Python/plateEncrypt/plateEncrypt.py
+++ Python/plateEncrypt/plateEncrypt.py
@@ -66,11 +66,12 @@ def main():
             decoded = buff.encrypted_frames[i]
             decoded_frame = cv2.imdecode(numpy.frombuffer(decoded, numpy.uint8), -1)
             outname = "decoded_" + str(i + 1) + ".jpg"
-            if decoded is None:
-                pass
-            print("Decoded_frame size: " + str(len(decoded_frame)))
-            cv2.imwrite(outname, decoded_frame)
-            buff.frames.append(decoded_frame)
+            try:
+                print("Decoded_frame size: " + str(len(decoded_frame)))
+                cv2.imwrite(outname, decoded_frame)
+                buff.frames.append(decoded_frame)
+            except TypeError:
+                print("Errored")
             # print("Writing picture to file...")
             # frame = buff.encrypted_frames[0]
             # print(frame)
