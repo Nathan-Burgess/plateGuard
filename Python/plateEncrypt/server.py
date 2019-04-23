a88a2240bfa63338244ca737b033ec3e82a852f7
Michael Nutt <michaelnutt2@my.unt.edu>
2019-04-23 13:28:53 -0500

    Trying something new, blame nathan if things blow up

--- Python/plateEncrypt/server.py
+++ Python/plateEncrypt/server.py
@@ -4,6 +4,7 @@ from Crypto.Cipher import ChaCha20
 from Crypto.Random import get_random_bytes
 import buffer
 import cv2
+import struct
 
 
 class Server:
@@ -29,54 +30,51 @@ class Server:
         print("Sending key...")
         client.sendall(self.key)
 
-    def receiveframes(self, client, buff):
-        total_data = []
+    def recv_msg(self, client):
+        raw_msglen = self.receiveframes(4)
+        if not raw_msglen:
+            return None
+        msglen = struct.unpack('>I', raw_msglen)[0]
+
+        return self.receiveframes(client, msglen)
+
+    def receiveframes(self, client, n):
         data = b''
 
-        # fsize = client.recv(1024)
-        # print(fsize.decode())
-        # count = 8192
-        #
-        # frame_size = int(fsize.decode())
-        #
-        # while frame_size:
-        #     if frame_size < count:
-        #         count = frame_size
-        #
-        #     newbuff = client.recv(count)
-        #     if not newbuff: break
-        #     data += newbuff
-        #     frame_size -= len(newbuff)
-        #
-        # buff.encrypted_frames.append(data)
+        while len(data) < n:
+            packet = client.recv(n - len(data))
+            if not packet:
+                return None
+            data += packet
+        return data
 
-        while True:
-            data = client.recv(8192)
-            if self.end in data:
-                total_data.append(data[:data.find(self.end)])
-                break
-            total_data.append(data)
-            if len(total_data) > 1:
-                # check if end_of_data was split
-                last_pair = total_data[-2] + total_data[-1]
-                if self.end in last_pair:
-                    total_data[-2] = last_pair[:last_pair.find(self.end)]
-                    total_data.pop()
-                    break
+        # while True:
+        #     data = client.recv(8192)
+        #     if self.end in data:
+        #         total_data.append(data[:data.find(self.end)])
+        #         break
+        #     total_data.append(data)
+        #     if len(total_data) > 1:
+        #         # check if end_of_data was split
+        #         last_pair = total_data[-2] + total_data[-1]
+        #         if self.end in last_pair:
+        #             total_data[-2] = last_pair[:last_pair.find(self.end)]
+        #             total_data.pop()
+        #             break
+        #
+        # frame = total_data[0]
+        # for part in total_data[1:]:
+        #     frame += part
 
-        frame = total_data[0]
-        for part in total_data[1:]:
-            frame += part
+        # print("Frame size: " + str(len(frame)))
+        # print(frame)
+        # buff.encrypted_frames.append(frame)
 
-        print("Frame size: " + str(len(frame)))
-        print(frame)
-        buff.encrypted_frames.append(frame)
-
-    def decryptframes(self, buff, i):
-        frame = buff.encrypted_frames[-1]
-        nounce = frame[:8]
+    def decryptframes(self, data, buff):
+        # frame = buff.encrypted_frames[-1]
+        nounce = data[:8]
         print(len(nounce))
-        ciphertext = frame[8:]
+        ciphertext = data[8:]
         cipher = ChaCha20.new(key=self.key, nonce=nounce)
         decoded = cipher.decrypt(ciphertext)
         print("Decoded size " + str(len(decoded)))
@@ -96,13 +94,14 @@ if __name__ == "__main__":
         print("Client connected from " + str(addr))
         s.handshake(client)
         for i in range(150):
-            s.receiveframes(client, buff)
+            data = s.receiveframes(client)
             print("Received frame " + str(i+1))
             # print("Writing picture to file...")
             # frame = buff.encrypted_frames[0]
             # print(frame)
             # cv2.imwrite("unencrypted.jpg", frame)
             print("Decrypting frame " + str(i+1))
-            s.decryptframes(buff, i)
+            # s.decryptframes(buff, i)
+            s.decryptframes(data, buff)
             client.sendall("halo".encode())
         client.close()
